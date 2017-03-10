5a87ffab0cbcc5c273542386696f88ebfa933c7e
Khalil Ayache <khalilayache@gmail.com>
2017-03-10 14:33:11 -0300

    Add internacionalization

--- app/src/main/java/com/khalilayache/table99/models/Person.java
+++ app/src/main/java/com/khalilayache/table99/models/Person.java
@@ -102,15 +102,4 @@ public class Person implements Parcelable {
         dest.writeString(bio);
         dest.writeString(id);
     }
-
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
 }
